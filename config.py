597cebf27b8be031b2d297f47a6818a393d969d8
santa <ryancurtis998@yahoo.com>
2019-04-13 11:00:47 +0300

    updated config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+
+class Config:
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?category={}&language=en&apiKey={}'
+
+    ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&language=en&apiKey={}'
+
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+
+
+class ProdConfig(Config):
+
+    pass
+
+
+class DevConfig(Config):
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
